6a95a8fca86e64413a92c95c91ddff0d62a06c69
guoxiaoyu543 <guoxiaoyu543@163.com>
2017-10-21 20:05:59 +0800

    add new tools: transcoding

---
+++ encoding/encode.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import glob
+
+os.chdir(sys.path[0])
+
+# todo output warning message to remind users to install enca on their machine.
+
+l_file_name = glob.glob('*.*')
+
+for s_file_name in l_file_name:
+    # backup
+    shutil.copyfile(s_file_name, ''.join(s_file_name, '.backup'))
+    # transcoding
+    os.system(''.join('enca -L zh_CN -x UTF-8 ', s_file_name))
+
