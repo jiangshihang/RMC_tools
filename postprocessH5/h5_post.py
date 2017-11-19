0eb7f5bd45e1ad5fa3664aa8a7d79cabe7b4a928
guoxiaoyu <guoxiaoyu543@163.com>
2017-11-19 22:28:09 +0800

    Refactor: add comments to h5 -> tecplot function

--- postprocessH5/h5_post.py
+++ postprocessH5/h5_post.py
@@ -9,15 +9,15 @@ import numpy as np
 os.chdir(sys.path[0])
 
 
-def get_array_from_h5(s_h5_name, s_array_name):
+def get_array_from_h5(s_h5_name, s_dataset_name):
     """
-
-    :param s_h5_name:
-    :param s_array_name:
-    :return:
+    get desired data from .h5 file
+    :param s_h5_name: name of the .h5 file
+    :param s_dataset_name: name of the dataset name
+    :return: numpy array maintaining desired dataset
     """
     f_h5 = h5.File(s_h5_name)
-    s_dataset_name = '/'.join(['STATE_0001', s_array_name])
+    s_dataset_name = '/'.join(['STATE_0001', s_dataset_name])
     np_property = f_h5[s_dataset_name]
 
     return np_property
@@ -28,7 +28,7 @@ def calc_assem_radial_ave(np_property, l_axial_location=[]):
 
     :param np_property:
     :param l_axial_location:
-    :return:
+    :return: numpy array
     """
     t_dimen = np_property.shape
 
@@ -105,10 +105,12 @@ def print_assem_radial_ave_sym(np_assem_radial_ave, n_axial_location, l_core_map
     f_assem_radial_ave_sym.close()
 
 
-def convert_np_to_tecplot(np_property, l_core_map):
+def write_np_to_tecplot(np_property, s_property_name, l_core_map):
     """
-
-    :param:
+    process and write numpy array to tecplot .dat file
+    :param: np_property: numpy array to be printed
+    :param s_property_name: name of the data
+    :param l_core_map: list of core map
     :return:
     """
     import math
@@ -130,16 +132,17 @@ def convert_np_to_tecplot(np_property, l_core_map):
                         np_tecplot[i_x_assem * n_x + i_x_rod, i_y_assem * n_y + i_y_rod, i_z] = \
                             (np_property[i_x_rod, i_y_rod, i_z, i_assem] * np_core_map[i_x_assem, i_y_assem])
 
-    write_tecplot_file(np_tecplot)
+    write_tecplot_file(np_tecplot, s_property_name)
     
 
-def write_tecplot_file(np_tecplot):
+def write_tecplot_file(np_tecplot, s_property_name):
     """
-
-    :param np_tecplot:
+    write numpy array to tecplot file
+    :param np_tecplot: numpy array to be printed
+    :param s_property_name: name of the data
     :return:
     """
-    s_title = 'property from hdf5'  # todo using more flexible variable
+    s_title = s_property_name
     n_x, n_y, n_z = np_tecplot.shape
 
     with open('tecplot', 'w') as f_tecplot:
@@ -182,8 +185,28 @@ def get_dimen_from_dat(s_dat_name):
     return l_dimen
 
 
+def read_core_map():
+    """
+    read core map from 'map' file
+    :return: list containing core map
+    """
+    import re
+
+    try:
+        f_core_map = open('map', 'r')
+    except IOError:
+        print 'ERROR: core map file does not exist!'
+
+    s_core_map = f_core_map.read()
+    f_core_map.close()
+    l_core_map = re.findall('\d', s_core_map)
+
+    return [int(n_map) for n_map in l_core_map]
+
+
 if __name__ == '__main__':
     from optparse import OptionParser
+    import re
 
     # command line parsing
     usage = """usage: %prog [options]"""
@@ -201,49 +224,38 @@ if __name__ == '__main__':
     if options.input_name is not None:
         s_data_name = options.input_name
     else:
-        s_data_name = 'pdeck.ctf_iter8.h5'
+        try:
+            f_h5_config = open('h5_config', 'r')
+            s_data_name = re.findall('[hH][dD][fF]5[nN][aA][mM][eE]\s*=\s*(.*)', f_h5_config.read())[-1]
+            f_h5_config.close()
+        except:
+            print 'WARNING: h5 file name not found!'
+            print 'The name is set to be pdeck.ctf.h5 by default.'
+            s_data_name = 'pdeck.ctf.h5'
 
     if options.property_name is not None:
         s_property_name = options.property_name
     else:
-        s_property_name = 'pin_powers'
+        try:
+            f_h5_config = open('h5_config', 'r')
+            s_property_name = re.findall('[dD][aA][tT][aA][sS][eE][tT]\s*=\s*(.*)', f_h5_config.read())[-1]
+            f_h5_config.close()
+        except:
+            print 'WARNING: dataset name not found!'
+            print 'The dataset name is set to be pin_fueltemps [C] by default.'
+            s_property_name = 'pin_powers'
 
     if options.data_type is not None:
         s_data_type = options.data_type.upper()
     else:
         s_data_type = 'HDF5'
 
-    # define core map
-    import re
-    f_core_map = open('map', 'r')
-    s_core_map = f_core_map.read()
-    f_core_map.close()
-    # s_core_map = '0 0 0 0 1 1 1 1 1 1 1 0 0 0 0 ' \
-    #              '0 0 1 1 1 1 1 1 1 1 1 1 1 0 0 ' \
-    #              '0 1 1 1 1 1 1 1 1 1 1 1 1 1 0 ' \
-    #              '0 1 1 1 1 1 1 1 1 1 1 1 1 1 0 ' \
-    #              '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ' \
-    #              '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ' \
-    #              '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ' \
-    #              '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ' \
-    #              '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ' \
-    #              '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ' \
-    #              '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ' \
-    #              '0 1 1 1 1 1 1 1 1 1 1 1 1 1 0 ' \
-    #              '0 1 1 1 1 1 1 1 1 1 1 1 1 1 0 ' \
-    #              '0 0 1 1 1 1 1 1 1 1 1 1 1 0 0 ' \
-    #              '0 0 0 0 1 1 1 1 1 1 1 0 0 0 0 '
-    l_core_map = re.findall('\d', s_core_map)
-    l_core_map = [int(n_map) for n_map in l_core_map]
+    l_core_map = read_core_map()
 
     # resolve h5 data
     if s_data_type == 'HDF5':
-
         np_property = get_array_from_h5(s_data_name, s_property_name)
-        # np_assem_ave_property = calc_assem_radial_ave(np_property, [49])
-        # print_assem_radial_ave_sym(np_assem_ave_property, 49, l_core_map)
-
-        convert_np_to_tecplot(np_property, l_core_map)
+        write_np_to_tecplot(np_property, s_property_name, l_core_map)
 
     elif s_data_type == 'DAT':
         print "DAT data resolution not supported for now!"
